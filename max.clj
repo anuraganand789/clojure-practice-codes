8cca37e07691827258e24bc852325bd0af1675e6
Anurag Anand <anuraganand789@gmail.com>
2018-12-24 20:48:14 +0530

    getting max value from a collection of numers using builtin max function of clojure

--- max.clj
+++ max.clj
@@ -1,9 +1,8 @@
-(ns clojure-projects-codes
-  (:gen-class))
-
 ;; functions in this file gets the max between two numbers and more than two numbers
 (defn max-of-two [num1 num2]
   (if (> num1 num2) num1 num2))
+
+(defn max-using-max [& args] (apply max args))
 
 (defn -main [& args]
   (str (max-of-two 12 13)))
