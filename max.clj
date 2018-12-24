f5da446495e6155d284839295c9a7b47a1cb9171
Anurag Anand <anuraganand789@gmail.com>
2018-12-24 20:53:33 +0530

    used recur to loop over elments in colleciton. And also used max-of-two methods to get the max of two elements

--- max.clj
+++ max.clj
@@ -4,5 +4,10 @@
 
 (defn max-using-max [& args] (apply max args))
 
+(defn max-of-coll [first-number & rest-of-the-numbers]
+  (if (= 0 (count rest-of-the-numbers))
+    first-number
+    (recur (max-of-two first-number (first rest-of-the-numbers)) (rest rest-of-the-numbers)))) 
+
 (defn -main [& args]
-  (str (max-of-two 12 13)))
+  (str (max-of-coll 16 12 13 14 15)))
