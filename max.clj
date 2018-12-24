62b7a48bab7ce78d9c95f1ccfef0b32fe51f9c47
Anurag Anand <anuraganand789@gmail.com>
2018-12-24 21:04:43 +0530

    using reduce with clojure max function and, in other implementation I used my max-of-two functions

--- max.clj
+++ max.clj
@@ -1,3 +1,6 @@
+;; collection used for tests
+(def numbers [1 2 3 11 -1 25 99])
+
 ;; functions in this file gets the max between two numbers and more than two numbers
 (defn max-of-two [num1 num2]
   (if (> num1 num2) num1 num2))
@@ -7,7 +10,15 @@
 (defn max-of-coll [first-number & rest-of-the-numbers]
   (if (= 0 (count rest-of-the-numbers))
     first-number
-    (recur (max-of-two first-number (first rest-of-the-numbers)) (rest rest-of-the-numbers)))) 
+    (recur
+     (max-of-two first-number (first rest-of-the-numbers))
+     (rest rest-of-the-numbers)))) 
+
+(defn max-using-reduce [all-elements]
+  (reduce max all-elements))
+
+(defn max-using-max-of-two [all-elements]
+  (reduce max-of-two all-elements))
 
 (defn -main [& args]
-  (str (max-of-coll 16 12 13 14 15)))
+  (max-using-max-of-two numbers))
